0a6c56da65c56227a835c153d76376d0ca2afd64
ayaanshsharma <98742867+ayaanshsharma@users.noreply.github.com>
2022-07-30 16:53:13 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+
+prediction_1 = "";
+prediction_2 = "";
+
+Webcam.set({
+    width:300,
+    height:300,
+    image_format:'png',
+    png_quality:90
+
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach("#camera");
+
+ function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="capture_image" src="'+data_uri+'"/>';
+    });
+ }
+
+ console.log('ml5 version' ,ml5.version);
+
+ Classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/p4FUq4c6T/model.json",modelLoaded);
+
+ function modelLoaded(){
+    console.log("modelLoaded");
+ }
